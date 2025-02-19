228126765afa1c12964b2bcf8d117509bf1f527a
Muhammad Ghathfan Raviansyah <muhammadghathfanraviansyah@gmail.com>
2025-02-19 14:11:41 +0700

    Update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledMerah = 8;
+const int ledKuning = 9;
+const int ledHijau = 10;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Mengatur pin-pin yang dihubungkan ke LED sebagai output.
+  // Hal ini berarti pin-pin tersebut dapat digunakan untuk mengendalikan LED.
+  pinMode(ledMerah, OUTPUT);
+  pinMode(ledKuning, OUTPUT);
+  pinMode(ledHijau, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // LED Merah
+  // Menyalakan LED Merah selama 2 detik, kemudian mematikannya.
+  digitalWrite(ledMerah, HIGH);
+  delay(6000);
+  digitalWrite(ledMerah, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // LED Kuning
+  // Menyalakan LED Kuning selama 2 detik, kemudian mematikannya.
+  digitalWrite(ledKuning, HIGH);
+  delay(500);
+  digitalWrite(ledKuning, LOW);
+
+  // LED Hijau
+  // Menyalakan LED Hijau selama 2 detik, kemudian mematikannya.
+  digitalWrite(ledHijau, HIGH);
+  delay(4000);
+  digitalWrite(ledHijau, LOW);
+
+  digitalWrite(ledKuning, HIGH);
+  delay(500);
+  digitalWrite(ledKuning, LOW);
 }
